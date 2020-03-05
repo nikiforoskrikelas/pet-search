b7966c0c78812ef2ade8c1fe092cd067d3b6f807
Nikiforos Krikelas <nk00322@surrey.ac.uk>
2020-03-05 12:37:05 +0000

    - bottom navigation bar - fragments for each view - icons for navigation bar

--- app/src/main/java/nk00322/surrey/petsearch/MainActivity.java
+++ app/src/main/java/nk00322/surrey/petsearch/MainActivity.java
@@ -1,11 +1,26 @@
 package nk00322.surrey.petsearch;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.MenuItem;
 
 import com.example.petsearch.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private static final String TAG = "MainActivity";
+
+    BottomNavigationView bottomNavigationView;
+    Organize organizeFragment = new Organize();
+    SearchParties searchPartiesFragment = new SearchParties();
+    Map mapFragment = new Map();
+    Notifications notificationsFragment = new Notifications();
+    Me meFragment = new Me();
 
 
     @Override
@@ -13,6 +28,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        bottomNavigationView = findViewById(R.id.bottom_nav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+        bottomNavigationView.setSelectedItemId(R.id.nav_map);
 
+    }
+
+    /**
+     * Called when an item in the bottom navigation menu is selected.
+     *
+     * @param item The selected item
+     * @return true to display the item as the selected item and false if the item should not be
+     * selected. Consider setting non-selectable items as disabled preemptively to make them
+     * appear non-interactive.
+     */
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        Log.i(TAG, "onNavigationItemReselected");
+
+        switch (item.getItemId()) {
+            case R.id.nav_organize:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.container, organizeFragment).commit();
+                return true;
+            case R.id.nav_search_parties:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.container, searchPartiesFragment).commit();
+                return true;
+            case R.id.nav_map:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.container, mapFragment).commit();
+                return true;
+            case R.id.nav_notifications:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.container, notificationsFragment).commit();
+                return true;
+            case R.id.nav_me:
+                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.fade_out).replace(R.id.container, meFragment).commit();
+                return true;
+        }
+
+        return false;
     }
 }
---
+++ app/src/main/java/nk00322/surrey/petsearch/Organize.java
@@ -0,0 +1,30 @@
+package nk00322.surrey.petsearch;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.petsearch.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Organize extends Fragment {
+
+    public Organize() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_organize, container, false);
+    }
+}
